066783f77bec0458ec6a6d711a8d15aa9556059a
khamphoud <123445047+khamphoud@users.noreply.github.com>
2026-03-22 18:41:44 +0700

    Add files via upload

---
+++ api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzWcEgQLav1sILR4nWJMtMruCYSTA1yE0lRAfPy2-VmNl-cnqjf26HE5LM7dqCRJ9siPQ/exec';
