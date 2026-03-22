dc4eeecf5b0d049a959a468605e5cb96b78a6831
khamphoud <123445047+khamphoud@users.noreply.github.com>
2026-03-22 18:52:34 +0700

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzWcEgQLav1sILR4nWJMtMruCYSTA1yE0lRAfPy2-VmNl-cnqjf26HE5LM7dqCRJ9siPQ/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxwCrEaPJbfmaTNaptkkfqZ9JIUdOcnOKz7ocGwFrHZApAX_H_8HHxwjkTtC52BqxgH/exec';
